7b54d8d4d014ee7f2c30b5706679d33bf8b708df
admin <admin@gmail.com>
2025-01-04 13:36:36 -0500

    Actualizar la sección de proyectos en works.html

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Analytics } from '@vercel/analytics/react';
+
+function App() {
+    return (
+        <div className="App">
+            {/* Tu contenido de la aplicación */}
+            <Analytics />
+        </div>
+    );
+}
+
+export default App;
